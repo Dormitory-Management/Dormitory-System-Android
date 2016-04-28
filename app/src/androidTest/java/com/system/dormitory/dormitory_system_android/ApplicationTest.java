c3f5edc6b69f398de03ce4b957bc623404457300
김보운 <haioer@naver.com>
2016-04-28 11:59:22 +0900

    start

---
+++ app/src/androidTest/java/com/system/dormitory/dormitory_system_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.system.dormitory.dormitory_system_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
