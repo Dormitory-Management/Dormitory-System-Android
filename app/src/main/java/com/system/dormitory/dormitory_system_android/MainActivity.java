32f5bc6fd5175eeb4222e14215afafea79ac362c
KimBoWoon <haioer@naver.com>
2016-05-06 15:55:44 +0900

    Success DrawerLayout

--- app/src/main/java/com/system/dormitory/dormitory_system_android/MainActivity.java
+++ app/src/main/java/com/system/dormitory/dormitory_system_android/MainActivity.java
@@ -1,13 +1,90 @@
 package com.system.dormitory.dormitory_system_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lvNavList;
+    private DrawerLayout dlDrawer;
+    private ActionBarDrawerToggle dtToggle;
+    private String[] navItems = {"점호", "대여", "외박", "공지사항", "게시판"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvNavList = (ListView) findViewById(R.id.lv_activity_main_nav_list);
+
+        lvNavList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navItems));
+        lvNavList.setOnItemClickListener(new DrawerItemClickListener());
+
+        dlDrawer = (DrawerLayout) findViewById(R.id.dl_activity_main_drawer);
+        dtToggle = new ActionBarDrawerToggle(this, dlDrawer, R.string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        dlDrawer.setDrawerListener(dtToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        dtToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        dtToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (dtToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+            switch (position) {
+                case 0:
+                    Toast.makeText(MainActivity.this, "점호", Toast.LENGTH_SHORT).show();
+                    break;
+                case 1:
+                    Toast.makeText(MainActivity.this, "대여", Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    Toast.makeText(MainActivity.this, "외박", Toast.LENGTH_SHORT).show();
+                    break;
+                case 3:
+                    Toast.makeText(MainActivity.this, "공지사항", Toast.LENGTH_SHORT).show();
+                    break;
+                case 4:
+                    Toast.makeText(MainActivity.this, "게시판", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            dlDrawer.closeDrawer(lvNavList);
+        }
     }
 }
