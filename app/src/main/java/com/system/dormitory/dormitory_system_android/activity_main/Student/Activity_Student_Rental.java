69af7e59495e62e062863850979198f5c24695c3
kisang <whrltkd789@naver.com>
2016-06-07 02:17:40 +0900

    student_rental layout

---
+++ app/src/main/java/com/system/dormitory/dormitory_system_android/activity_main/Student/Activity_Student_Rental.java
@@ -0,0 +1,28 @@
+package com.system.dormitory.dormitory_system_android.activity_main.Student;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.androidquery.AQuery;
+import com.system.dormitory.dormitory_system_android.R;
+import com.system.dormitory.dormitory_system_android.login.Activity_Login;
+
+/**
+ * Created by Administrator on 2016-06-07.
+ */
+public class Activity_Student_Rental extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Activity_Login login = (Activity_Login) Activity_Login.login_Activity; //login_Activity_finish
+        login.finish();
+
+
+
+
+    }
+
+
+}
